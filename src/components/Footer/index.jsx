e553ff84d7d98d4ebf04034ca6f1b253f1713098
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-09-19 20:05:28 +0100

    footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,64 @@
 import React from "react";
-// import "../../styles/Footer.scss";
+import "../../styles/Footer.scss";
+
 export default function Footer() {
   return (
-    <footer className="Footer">
-      <div className="F_text">
-        <p>Copyright © 2023 Lexxy FX. All rights reserved.</p>
-        <p>
-          Designed by <span className="F_Team">Retro Developers</span>
-        </p>
+    <footer className="footer_container">
+      <div className="footer_wrapper">
+        <div className="footer_section">
+          <h3 className="footer_title">Get Connected</h3>
+
+          <p>
+            Join the conversation on social, and stay connected with our latest
+            events and partners around the world.
+          </p>
+
+          <div className="footer_social">
+            <a href="https://www.facebook.com/">
+              <img src="" alt="" />
+            </a>
+            <a href="https://www.instagram.com/">
+              <img src="" alt="" />
+            </a>
+            <a href="https://www.twitter.com/">
+              <img src="" alt="" />
+            </a>
+            <a href="https://www.linkedin.com/">
+              <img src="" alt="" />
+            </a>
+          </div>
+        </div>
+
+        <div className="footer_section">
+          <h3 className="footer_title">Contact</h3>
+
+          <div className="footer_contact">
+            <img src="" alt="" />
+            <p>Ojay Street, Kreg Ave, Lagos, Nigeria</p>
+          </div>
+          <div className="footer_contact">
+            <img src="" alt="" />
+            <p>Ojay Street, Kreg Ave, Lagos, Nigeria</p>
+          </div>
+          <div className="footer_contact">
+            <img src="" alt="" />
+            <p>Ojay Street, Kreg Ave, Lagos, Nigeria</p>
+          </div>
+        </div>
+
+        <div className="footer_section">
+          <h3 className="footer_title">Stay Informed</h3>
+
+          <p>
+            Subscribe to our newsletter to receive updates of latest news and
+            events.
+          </p>
+
+          <form className="footer_form">
+            <input type="text" placeholder="Enter your email" />
+            <button type="submit">Subscribe</button>
+          </form>
+        </div>
       </div>
     </footer>
   );
