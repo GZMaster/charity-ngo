4be81eb62fea2a5d02eb6b7ee968205a4a024702
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-09-22 06:14:16 +0100

    goal data

--- src/assets/data/GoalData.js
+++ src/assets/data/GoalData.js
@@ -8,25 +8,25 @@ const GoalData = [
     id: 1,
     image: support,
     title: "Fostering Hope and Leadership",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    text: "Our goal is to instill hope and develop the personality and leadership potential of disillusioned individuals and adolescents. Through guidance and mentorship, we will help them lead purposeful and value-oriented lives.",
   },
   {
     id: 2,
     image: prayers,
     title: "Empowering People with Disabilities",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    text: "We are committed to identifying and supporting people with disabilities, helping them lead fulfilling lives, and reducing the burden they place on their families and society.",
   },
   {
     id: 3,
     image: volunteers,
     title: "Empowering the Less Privileged and Supporting Indigent Students",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    text: "Our primary goal is to empower less privileged individuals by identifying their talents and providing them with the necessary environment and opportunities to realize their potential.",
   },
   {
     id: 4,
     image: donations,
     title: "Community Development and Youth Empowerment",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    text: "Our goal is to uplift marginalized communities, offer scholarships to deserving youth, initiate innovative socioeconomic programs, and connect them to opportunities for social and economic empowerment.",
   },
 ];
 
