a9c6b6a9d05df9a844e6e9a71fafbe1f91b0fb61
majirih <williamsagbanoma@gmail.com>
2023-09-22 04:50:43 +0100

    reviewed jsx and styles

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import GoalCard from "../components/Card/GoalCard";
 import GoalData from "../assets/data/GoalData";
 import "../styles/IntroPage.scss";
-import Logo from "../assets/logo.svg";
 
 const IntroPage = () => {
   return (
     <div className="intropage_container">
       <div className="intropage_wrapper">
         <div className="intropage_title">
-          <h3>
-            WELCOME TO Zero Hunger <img src={Logo} alt="logo" />
-          </h3>
+          <h3>WELCOME TO Zero Hunger</h3>
           <h1>
             We Believe that we can Eradicate <br /> <span>Poverty</span> with
             you
