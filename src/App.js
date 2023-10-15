7c458fe5d547ad895306fa16be327f078186985c
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-10-15 19:04:46 +0100

    mobile styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/index";
 import NavBar from "./components/Navbar/index";
+import "./styles/App.scss";
 
 export default function App() {
   return (
